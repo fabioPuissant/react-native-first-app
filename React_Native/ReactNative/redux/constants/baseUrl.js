57e52a4aaf627f28285d8b220df8264c786d6b3b
Michael Geuens <michaelgeuensbe@gmail.com>
2019-12-18 05:25:17 +0900

    changed static ip to base_url in ticketAction

--- React_Native/ReactNative/redux/constants/baseUrl.js
+++ React_Native/ReactNative/redux/constants/baseUrl.js
@@ -1,3 +1,1 @@
-export const BASE_URL = 'http://10.83.137.171:8000';
-//michael: 'http://10.83.137.171:8000';
-//fabio: 'http://192.168.0.114:8000';
+export const BASE_URL = 'http://192.168.1.51:8000';
--- React_Native/ReactNative/screens/Home.js
+++ React_Native/ReactNative/screens/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import RoomsList from '../components/rooms/RoomsList';
+import RoomScreen from '../screens/RoomScreen';
+import TicketList from '../components/tickets/TicketList';
 
 const Home = props => {
   return <RoomsList navigation={props.navigation} />;
