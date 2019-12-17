208103e2f7737a544b71350516097cb45eb026b8
Michael-Meeus <Michael.meeus@gmail.com>
2019-12-17 23:09:51 +0100

    WORKING, with new DB for imageUrls

--- wp1DB.sql
+++ wp1DB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Gegenereerd op: 16 dec 2019 om 10:23
+-- Gegenereerd op: 17 dec 2019 om 21:57
 -- Serverversie: 5.7.27-0ubuntu0.16.04.1
 -- PHP-versie: 7.3.8-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -38,7 +38,11 @@ CREATE TABLE `asset` (
 
 INSERT INTO `asset` (`id`, `roomId`, `name`) VALUES
 (1, 1, 'Beamer'),
-(2, 2, 'Blackboard');
+(2, 2, 'Blackboard'),
+(3, 1, 'Silver screen'),
+(4, 1, 'Blackboard'),
+(5, 1, 'Tv'),
+(6, 1, 'Video player');
 
 -- --------------------------------------------------------
 
@@ -49,18 +53,19 @@ INSERT INTO `asset` (`id`, `roomId`, `name`) VALUES
 CREATE TABLE `room` (
   `id` int(11) NOT NULL,
   `name` varchar(250) NOT NULL,
-  `happinessScore` int(1) NOT NULL
+  `happinessScore` int(1) NOT NULL,
+  `imageUrl` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `room`
 --
 
-INSERT INTO `room` (`id`, `name`, `happinessScore`) VALUES
-(1, 'B051', 3451),
-(2, 'ISpace07', 20),
-(3, 'BlackBox', 50),
-(4, 'B243', 2);
+INSERT INTO `room` (`id`, `name`, `happinessScore`, `imageUrl`) VALUES
+(1, 'B051', 3451, 'https://images.pexels.com/photos/1181395/pexels-photo-1181395.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(2, 'ISpace07', 20, 'https://images.pexels.com/photos/1708936/pexels-photo-1708936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(3, 'BlackBox', 50, 'https://images.pexels.com/photos/416320/pexels-photo-416320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+(4, 'B243', 2, 'https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 
 -- --------------------------------------------------------
 
@@ -83,7 +88,7 @@ INSERT INTO `ticket` (`id`, `assetId`, `numberOfVotes`, `description`) VALUES
 (1, 1, 5, 'Beamer geeft geen correcte kleuren'),
 (2, 1, 0, 'The PC doesn\'t start.'),
 (3, 1, 0, 'The PC doesn\'t start.'),
-(4, 1, 0, 'The PC doesn\'t start.');
+(4, 2, 0, 'The PC doesn\'t start.');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -115,7 +120,7 @@ ALTER TABLE `ticket`
 -- AUTO_INCREMENT voor een tabel `asset`
 --
 ALTER TABLE `asset`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT voor een tabel `room`
 --
