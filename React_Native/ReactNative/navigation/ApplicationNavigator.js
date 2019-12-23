b10c92c39b5893486cd93750977128ebab8b816f
Michael Geuens <michaelgeuensbe@gmail.com>
2019-12-23 18:36:19 +0900

    finetuned hamburger menu

--- React_Native/ReactNative/navigation/ApplicationNavigator.js
+++ React_Native/ReactNative/navigation/ApplicationNavigator.js
@@ -1,29 +1,34 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+
+
 import Home from '../screens/Home';
 import RoomScreen from '../screens/RoomScreen';
 import TicketAddScreen from '../screens/TicketAddScreen';
 import AssetScreen from '../screens/AssetScreen';
+import TicketScreen from '../screens/TicketScreen';
 
-// Config different screens here
 const ApplicationNavigator = createStackNavigator(
   {
     HomeScreen: Home,
     RoomScreen: RoomScreen,
     TicketAddScreen: TicketAddScreen,
-    AssetScreen: AssetScreen
-  },
-  {
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false
-    }
+    AssetScreen: AssetScreen,
+    TicketScreen: TicketScreen
   }
 );
 
-const MainNavigator = createDrawerNavigator({
-  application: ApplicationNavigator
-});
+const MainNavigator = createDrawerNavigator(
+  {
+    application: ApplicationNavigator
+  },
+  {
+    navigationOptions: {
+      headerVisible: true,
+    }
+  }
+);
 
 export default createAppContainer(MainNavigator);
