3520ace8d9a66f7b5718c2a903150aa167f76985
Michael Geuens <michaelgeuensbe@gmail.com>
2019-12-24 03:21:31 +0900

    edited hamburger menu, to do: implementing logic

--- React_Native/ReactNative/navigation/ApplicationNavigator.js
+++ React_Native/ReactNative/navigation/ApplicationNavigator.js
@@ -1,34 +1,48 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
-
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import Home from '../screens/Home';
 import RoomScreen from '../screens/RoomScreen';
 import TicketAddScreen from '../screens/TicketAddScreen';
 import AssetScreen from '../screens/AssetScreen';
 import TicketScreen from '../screens/TicketScreen';
-
-const ApplicationNavigator = createStackNavigator(
-  {
-    HomeScreen: Home,
-    RoomScreen: RoomScreen,
-    TicketAddScreen: TicketAddScreen,
-    AssetScreen: AssetScreen,
-    TicketScreen: TicketScreen
-  }
-);
+import HeaderButton from '../components/HeaderButton';
+import colors from '../constants/Colors';
 
 const MainNavigator = createDrawerNavigator(
   {
-    application: ApplicationNavigator
+    HomeScreen: {
+      screen: Home
+    },
+    RoomScreen: {
+      screen: RoomScreen
+    },
+    TicketAddScreen: TicketAddScreen,
+    AssetScreen: AssetScreen,
+    TicketScreen: TicketScreen
   },
   {
-    navigationOptions: {
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Rooms Management App',
+      headerLeft:
+        (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item title="menu" iconName="ios-menu" onPress={() => {
+            navigation.toggleDrawer();
+          }} />
+        </HeaderButtons>),
+      headerStyle: {
+        backgroundColor: colors.primaryColor
+      },
+      headerTintColor: 'white',
       headerVisible: true,
-    }
+    })
   }
 );
 
-export default createAppContainer(MainNavigator);
+const ApplicationNavigator = createStackNavigator({ RootStack: { screen: MainNavigator } })
+
+export default createAppContainer(ApplicationNavigator);
