a119a3647397b568b8f0fe3270da8167760b5d51
Fabio_Puissant <fabio.puissant@hotmail.com>
2019-12-27 14:39:59 +0100

    Added Fade in view

--- React_Native/ReactNative/components/assets/AssetGrid.js
+++ React_Native/ReactNative/components/assets/AssetGrid.js
@@ -3,44 +3,52 @@ import { connect } from 'react-redux';
 import { View, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import { getTickets } from '../../redux/actions/ticketActions';
 import AssetItem from './AssetItem';
-
+import FadeInView from '../../layout/FadeInView';
 import {
   setCurrentAsset,
   findAssetsOfRoom,
   clearCurrentAsset
 } from '../../redux/actions/assetActions';
 
-const AssetGrid = ({ assets, navigation, ticket: { allTickets }, getTickets }) => {
+const AssetGrid = ({
+  assets,
+  navigation,
+  ticket: { allTickets },
+  getTickets
+}) => {
   useEffect(() => {
     getTickets();
   }, []);
   return (
-    <FlatList
-      style={styles.container}
-      data={assets}
-      renderItem={itemData => {
-        return (
-          <AssetItem
-            key={itemData.id}
-            index={itemData.index}
-            asset={itemData.item}
-            tickets={allTickets.filter(t => t.assetId === itemData.item.id)}
-            navigation={navigation}
-            setCurrentAsset={setCurrentAsset}
-          />
-        );
-      }}
-      numColumns={2}
-    />
+    <FadeInView style={styles.container}>
+      <FlatList
+        data={assets}
+        renderItem={itemData => {
+          return (
+            <AssetItem
+              key={itemData.id}
+              index={itemData.index}
+              asset={itemData.item}
+              tickets={allTickets.filter(t => t.assetId === itemData.item.id)}
+              navigation={navigation}
+              setCurrentAsset={setCurrentAsset}
+            />
+          );
+        }}
+        numColumns={2}
+      />
+    </FadeInView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { padding: 20 }
+  container: { padding: 20, flex: 1 }
 });
 
 const mapStateToProps = state => ({
   ticket: state.ticket
 });
 
-export default connect(mapStateToProps, { getTickets, setCurrentAsset })(AssetGrid);
+export default connect(mapStateToProps, { getTickets, setCurrentAsset })(
+  AssetGrid
+);
--- React_Native/ReactNative/components/assets/AssetItem.js
+++ React_Native/ReactNative/components/assets/AssetItem.js
@@ -4,7 +4,14 @@ import { Title, Paragraph, Card, Button } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { setCurrentAsset } from '../../redux/actions/assetActions';
 
-const AssetItem = ({ tickets, asset, assetState: { current }, navigation, index, setCurrentAsset }) => {
+const AssetItem = ({
+  tickets,
+  asset,
+  assetState: { current },
+  navigation,
+  index,
+  setCurrentAsset
+}) => {
   const [displayTickets, setDisplayTickets] = useState(null);
 
   useEffect(() => {
@@ -18,37 +25,39 @@ const AssetItem = ({ tickets, asset, assetState: { current }, navigation, index,
   };
 
   return (
-    <Card style={[styles.item, index % 2 === 0 ? styles.even : styles.odd]}>
-      <View style={styles.imgContainer}>
-        <Image
-          fadeDuration={400}
-          source={{
-            uri:
-              'https://images.pexels.com/photos/114820/pexels-photo-114820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-          }}
-          style={styles.image}
-          resizeMode='cover'
-        />
-      </View>
-      <View style={styles.whiteText}>
-        <Title style={{ color: '#FFF', fontSize: 15 }}>{asset.name}</Title>
+    <View style={{ flex: 1 }}>
+      <Card style={styles.item}>
+        <View style={styles.imgContainer}>
+          <Image
+            fadeDuration={400}
+            source={{
+              uri:
+                'https://images.pexels.com/photos/114820/pexels-photo-114820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+            }}
+            style={styles.image}
+            resizeMode='cover'
+          />
+        </View>
+        <View style={styles.whiteText}>
+          <Title style={{ color: '#FFF', fontSize: 15 }}>{asset.name}</Title>
 
-        <Paragraph style={styles.whiteText}>{'Id: ' + asset.id}</Paragraph>
-        <Paragraph style={styles.whiteText}>
-          {'Tickets: ' + (displayTickets ? displayTickets.length : 0)}
-        </Paragraph>
-      </View>
-      <Card.Actions>
-        <Button
-          mode={'contained'}
-          labelStyle={styles.ticketButton}
-          compact={true}
-          onPress={handleAssetNavigation}
-        >
-          See tickets
-      </Button>
-      </Card.Actions>
-    </Card>
+          <Paragraph style={styles.whiteText}>{'Id: ' + asset.id}</Paragraph>
+          <Paragraph style={styles.whiteText}>
+            {'Tickets: ' + (displayTickets ? displayTickets.length : 0)}
+          </Paragraph>
+        </View>
+        <Card.Actions>
+          <Button
+            mode={'contained'}
+            labelStyle={styles.ticketButton}
+            compact={true}
+            onPress={handleAssetNavigation}
+          >
+            See tickets
+          </Button>
+        </Card.Actions>
+      </Card>
+    </View>
   );
 };
 
@@ -66,7 +75,7 @@ const styles = StyleSheet.create({
   odd: {},
   item: {
     flex: 1,
-    margin: 15,
+    margin: 10,
     padding: 20,
     justifyContent: 'center',
     alignItems: 'center',
@@ -103,4 +112,4 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, {
   setCurrentAsset
-})(AssetItem);;
+})(AssetItem);
