9cd5daf517dd826af82768a894e6fdf3f5b2b502
Fabio_Puissant <fabio.puissant@hotmail.com>
2019-12-27 14:33:27 +0100

    Added FADE IN VIEW

--- React_Native/ReactNative/components/assets/AssetGrid.js
+++ React_Native/ReactNative/components/assets/AssetGrid.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { View, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import { getTickets } from '../../redux/actions/ticketActions';
 import AssetItem from './AssetItem';
+import FadeInView from '../../layout/FadeInView';
 
 import {
   setCurrentAsset,
@@ -20,23 +21,24 @@ const AssetGrid = ({
     getTickets();
   }, []);
   return (
-    <FlatList
-      style={styles.container}
-      data={assets}
-      renderItem={itemData => {
-        return (
-          <AssetItem
-            key={itemData.id}
-            index={itemData.index}
-            asset={itemData.item}
-            tickets={allTickets.filter(t => t.assetId === itemData.item.id)}
-            navigation={navigation}
-            setCurrentAsset={setCurrentAsset}
-          />
-        );
-      }}
-      numColumns={2}
-    />
+    <FadeInView style={styles.container}>
+      <FlatList
+        data={assets}
+        renderItem={itemData => {
+          return (
+            <AssetItem
+              key={itemData.id}
+              index={itemData.index}
+              asset={itemData.item}
+              tickets={allTickets.filter(t => t.assetId === itemData.item.id)}
+              navigation={navigation}
+              setCurrentAsset={setCurrentAsset}
+            />
+          );
+        }}
+        numColumns={2}
+      />
+    </FadeInView>
   );
 };
 
