ef3cb75b95d6d5289cfd3a30e07456871bf21fa5
Fabio_Puissant <fabio.puissant@hotmail.com>
2020-01-30 16:17:45 +0100

    Cleared out project :v:

--- README.md
+++
@@ -1,5 +0,0 @@
-# wp1-groep14
-wp1-groep14 created by GitHub Classroom
-
-# sensor:geolocation
-using the google static maps api and MapViews of react-native-maps to show the location of the user and rooms.
--- config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-];
